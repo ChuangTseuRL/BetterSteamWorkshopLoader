bd0fe36ad44a291153d967406492bea5a035c78e
ChuangTseuRL <gan-ning@hotmail.fr>
2021-11-12 01:46:15 +0000

    Remove IS_DEV_ON_EPIC define for public release.

--- BetterSteamWorkshopLoader/version.h
+++ BetterSteamWorkshopLoader/version.h
@@ -2,7 +2,7 @@
 #define VERSION_MAJOR 1
 #define VERSION_MINOR 0
 #define VERSION_PATCH 0
-#define VERSION_BUILD 181
+#define VERSION_BUILD 182
 
 #define stringify(a) stringify_(a)
 #define stringify_(a) #a
